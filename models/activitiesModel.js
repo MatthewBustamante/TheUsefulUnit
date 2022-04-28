078cfbb76b78ac1b5883d252f08c6d5f128623a4
Benjamin Morelli-Kirshner <wildfirex00@gmail.com>
2022-04-28 18:33:01 -0400

    Created the activity model, created a connection, export the connection.

--- models/activitiesModel.js
+++ models/activitiesModel.js
@@ -1,3 +1,10 @@
 const DATABASES = require("../utilities/databases");
+const validator = require("../utilities/validation");
+const logger = require("../utilities/logger");
 
 let connection = DATABASES.connection;
+
+module.exports = {
+    initialize,
+    connection
+}
