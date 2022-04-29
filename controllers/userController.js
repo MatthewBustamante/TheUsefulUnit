4c76e8c51de01ef1498ba4416f6a52ae214647be
jco898 <62203980+jco898@users.noreply.github.com>
2022-04-29 12:04:50 -0400

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,94 @@
 const express = require('express');
+const logger = require('../logger');
+const model = require('../models/usersModel');
 const router = express.Router();
 const routeRoot = '/';
-const logger = require('../logger');
 
 /**
- * Renders the login page
+ * Handles GET '/register'
+ * Directs the user to the sign-up page.
+ * @param {*} request 
+ * @param {*} response 
  */
-function showLoginForm(request, response) {
-    logger.info("User controller called (Login page)")
+async function showRegisterForm(request, response) {
 
-    response.render('login.hbs');
 }
+router.get('/register', showRegisterForm);
 
-router.get("/login", showLoginForm);
+/**
+ * Handles POST '/register'
+ * Creates a new user account.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function createUser(request, response) {
+
+}
+router.post('/register', createUser);
+
+/**
+ * Handles GET '/login'
+ * Directs the user to the login form.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function showLoginForm(request, response) {
+
+}
+router.get('/login', showLoginForm);
+
+/**
+ * Handles POST '/login'
+ * Authenticates the user when logging in.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function authenticateUser(request, response) {
+
+}
+router.post('/login', authenticateUser);
+
+/**
+ * Handles GET '/user'
+ * Redirects to the user account page.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function showUser(request, response) {
+
+}
+router.get('/user', showUser);
+
+/**
+ * Handles PUT '/users/:id'
+ * Calls the model to update user account settings.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function updateUser(request, response) {
+
+}
+router.put('/users/:id', updateUser);
+
+/**
+ * Handles DELETE '/users/:id'
+ * Calls the model to delete a user account.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function deleteUser(request, response) {
+
+}
+router.delete('/users/:id', deleteUser);
 
 module.exports = {
     router,
-    routeRoot
+    routeRoot,
+    showRegisterForm,
+    createUser,
+    showLoginForm,
+    authenticateUser,
+    showUser,
+    updateUser,
+    deleteUser
 }
