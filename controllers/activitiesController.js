7f6ec9160de28cb991786f7049e7fd4a0cb60f20
jco898 <62203980+jco898@users.noreply.github.com>
2022-04-28 22:12:21 -0400

    Create activitiesController.js
    Beginning of activitiesController.js implementation.

---
+++ controllers/activitiesController.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { P } = require('pino');
+const model = require('../models/activitiesModel');
+const router = express.Router();
+const routeRoot = '/';
+
+/**
+ * Handles POST '/activity'
+ * Calls the model to create a new activity.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function createActivity(request, response) {
+
+}
+router.post('/activity', createActivity);
+
+/**
+ * Handles GET '/activity/:id'
+ * Calls the model to show a single activity.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function showActivity(request, response) {
+
+}
+router.get('/activity/:id', showActivity);
+
+/**
+ * Handles GET '/activities'
+ * Calls the model to show all activities.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function showAllActivities(request, response) {
+
+}
+router.get('/activities', showAllActivities);
+
+/**
+ * Handles DELETE '/activities/:id'
+ * Calls the model to delete an activity.
+ * @param {*} request 
+ * @param {*} response 
+ */
+async function deleteActivity(request, response) {
+
+}
+router.delete('/activities/:id', deleteActivity);
+
+module.exports = {
+    router,
+    routeRoot,
+    createActivity,
+    showActivity,
+    showAllActivities,
+    deleteActivity
+}
