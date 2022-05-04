b7dbc5d1dab9827e36d96f51b0773ca483b43aa7
Benjamin Morelli-Kirshner <wildfirex00@gmail.com>
2022-05-03 23:39:32 -0400

    Initialize

--- models/activitiesModel.js
+++ models/activitiesModel.js
@@ -2,7 +2,7 @@ const DATABASES = require("../utilities/databases");
 const validator = require("../utilities/validation");
 const logger = require("../utilities/logger");
 
-let connection = DATABASES.connection;
+let connection = DATABASES.getConnection();
 
 module.exports = {
     connection
