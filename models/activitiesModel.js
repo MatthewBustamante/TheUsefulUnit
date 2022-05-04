3c877f5887a70c423358b608cfd07b32cfe8fe08
MatthewBustamante <78222417+MatthewBustamante@users.noreply.github.com>
2022-05-03 20:47:46 -0400

    Added the all the CRUD of the activity model. Just need to test it to make sure that everything works fine

--- models/activitiesModel.js
+++ models/activitiesModel.js
@@ -1,9 +1,142 @@
 const DATABASES = require("../utilities/databases");
 const validator = require("../utilities/validation");
+const ERRORS = require("../utilities/errors");
 const logger = require("../utilities/logger");
-
 let connection = DATABASES.connection;
 
+/**
+ * Creates a new activity in the database
+ * @param {*} name name of the new activity
+ * @param {*} description description of the new activity
+ * @param {*} startTime start time of the new activity
+ * @param {*} endTime end time of the new activity
+ * @param {*} ownerID the id of the owner of the new activity
+ * @returns the new activity object
+ */
+async function createActivity(name, description, startTime, endTime, ownerID) {
+    //create a new activity in the database
+    const sqlQuery = `INSERT INTO Activities (Name, Description, StartTime, EndTime, OwnerID) VALUES ('${name}', '${description}', '${startTime}', '${endTime}', '${ownerID}')`;
+    try {
+        await connection.execute(sqlQuery);
+        logger.info("Activity created");
+    } catch (error) {
+        logger.error(error);
+        throw error;
+    }
+    //return the new activity Created
+    const sqlQuery2 = `SELECT ActivityID FROM Activities WHERE Name = '${name}' AND Description = '${description}' AND StartTime = '${startTime}' AND EndTime = '${endTime}' AND OwnerID = '${ownerID}'`;
+    try {
+        const activityID = await connection.execute(sqlQuery2);
+        logger.info("Id of the new activity created retrieved");
+        return { "id": activityID, "name": name, "description": description, "startTime": startTime, "endTime": endTime, "ownerID": ownerID };
+    }
+    catch (error) {
+        logger.error(error);
+        throw error;
+    }
+}
+
+/**
+ * Gets the activity with the given id
+ * @param {*} activityID id of the activity to fetch
+ * @returns the activity with the given id
+ */
+async function getOneActivity(activityID) {
+    const sqlQuery = `SELECT * FROM Activities WHERE ActivityID = ${activityID}`;
+    try {
+        const result = await connection.execute(sqlQuery);
+        return result[0][0];
+    } catch (error) {
+        logger.error(error);
+        throw error;
+    }
+}
+
+/**
+ * Gets all the activities in the database
+ * @returns all the activities in the database
+ */
+async function getAllActivities() {
+    const sqlQuery = `SELECT * FROM Activities`;
+    try {
+        const result = await connection.execute(sqlQuery);
+        return result[0];
+    } catch (error) {
+        logger.error(error);
+        throw error;
+    }
+}
+
+/**
+ * Gets all the users participating in the given activity
+ * @param {*} activityID id of the activity
+ * @returns all the users in the given activity
+ */
+async function getUsersInActivity(activityID) {
+    const sqlQuery = `SELECT Username FROM Users WHERE UserID IN (SELECT UserID from UserActivity where ActivityID = ${activityID})`;
+    try {
+        const result = await connection.execute(sqlQuery);
+        return result[0];
+    } catch (error) {
+        logger.error(error);
+        throw error;
+    }
+}
+
+/**
+ * Adds the given user to the given activity
+ * @param {*} userID id of the user to add to the activity
+ * @param {*} activityID id of the activity to add the user to
+ */
+async function addUserToActivity(userID, activityID){
+    const sqlQuery = `INSERT INTO UserActivity (UserID, ActivityID) VALUES (${userID}, ${activityID})`;
+    try {
+        await connection.execute(sqlQuery);
+        logger.info("User added to the given activity");
+    } catch (error) {
+        logger.error(error);
+        throw error;
+    }
+}
+
+/**
+ * Deletes the user from the given activity
+ * @param {*} userID id of the user to delete from the activity
+ * @param {*} activityID id of the activity to delete the user from
+ */
+async function deleteUserFromActivity(userID, activityID){
+    const sqlQuery = `DELETE FROM UserActivity WHERE UserID = ${userID} AND ActivityID = ${activityID}`;
+    try {
+        await connection.execute(sqlQuery);
+        logger.info("User deleted from the given activity");
+    } catch (error) {
+        logger.error(error);
+        throw error;
+    }
+}
+
+/**
+ * Deletes the activity with the given id
+ * @param {*} activityID id of the activity to delete
+ */
+async function deleteActivity(activityID){
+    const sqlQuery = `DELETE FROM Activities WHERE ActivityID = ${activityID}`;
+    try {
+        await connection.execute(sqlQuery);
+        logger.info("Activity deleted");
+    } catch (error) {
+        logger.error(error);
+        throw error;
+    }
+}
+
 module.exports = {
-    connection
+    connection,
+    createActivity,
+    getOneActivity,
+    getAllActivities,
+    getUsersInActivity,
+    addUserToActivity,
+    deleteUserFromActivity,
+    deleteActivity
 }
