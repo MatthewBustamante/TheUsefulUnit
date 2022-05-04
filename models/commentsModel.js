05d6ec3cf86885680d5e1424567f00ee7c1740d8
Benjamin Morelli-Kirshner <wildfirex00@gmail.com>
2022-05-03 23:52:41 -0400

    Initialize

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -1,3 +1,11 @@
 const DATABASES = require("../utilities/databases");
+const validator = require("../utilities/validation");
+const ERRORS = require("../utilities/errors");
+const logger = require('../logger');
 
-let connection = DATABASES.connection;
+let connection = DATABASES.getConnection();
+
+
+module.exports = {
+    connection,
+};
