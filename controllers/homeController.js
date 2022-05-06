aeb2e6d04cf8fed1299f8862dca49248dbaf5595
Abdel <abdessalam.aithaqi@gmail.com>
2022-05-06 16:32:58 -0400

    Separate About and the home controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,35 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const routeRoot = '/';
-const logger = require('../logger');
+const routeRoot = "/";
+const logger = require("../logger");
 
 /**
  * Renders the home page
  */
 function showHome(request, response) {
-    logger.info("Home controller called");
+  logger.info("Home controller called");
 
-    response.render('home.hbs');
+  response.render("home.hbs");
 }
 
 router.get(routeRoot, showHome);
 router.get("/home", showHome);
 
-/**
- * Handles GET '/about'
- * Redirects the user to an about page.
- */
- function showAbout(request, response) {
-    logger.info("Showing About page");
-
-    response.render('about.hbs');
-}
-
-router.get("/about", showAbout);
-
 module.exports = {
-    router,
-    routeRoot,
-    showHome,
-    showAbout
-}
+  router,
+  routeRoot,
+  showHome,
+};
