ca0e9a8823a4baba0bfa8525234c73edc805b8c1
Abdel <abdessalam.aithaqi@gmail.com>
2022-05-16 12:08:28 -0400

    Delete comment implemented

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -1,7 +1,7 @@
 const DATABASES = require("../utilities/databases");
 const validator = require("../utilities/validation");
 const ERRORS = require("../utilities/errors");
-const logger = require('../logger');
+const logger = require("../logger");
 
 /**
  * Creates a new coment and adds it to the database
@@ -11,37 +11,65 @@ const logger = require('../logger');
  * @returns the comment object
  */
 async function createComment(userID, activityID, comment) {
-    let connection = DATABASES.getConnection();
+  let connection = DATABASES.getConnection();
 
-    logger.info("Comment model called (Create)");
+  logger.info("Comment model called (Create)");
 
-    if (comment == undefined || !validator.isValidComment(comment)) {
-        logger.error("Invalid comment");
+  if (comment == undefined || !validator.isValidComment(comment)) {
+    logger.error("Invalid comment");
 
-        throw new ERRORS.ValidationError("Invalid comment: '" + comment + "'");
-    }
+    throw new ERRORS.ValidationError("Invalid comment: '" + comment + "'");
+  }
 
-    //Get today's date
-    var today = new Date().toISOString().slice(0, 19).replace('T', ' ');
+  //Get today's date
+  var today = new Date().toISOString().slice(0, 19).replace("T", " ");
 
-    var comment = {
-        userID: userID,
-        activityID: activityID,
-        value: comment,
-        date: today
-    }
+  var comment = {
+    userID: userID,
+    activityID: activityID,
+    value: comment,
+    date: today,
+  };
 
-    await connection.execute(`INSERT INTO Comments (UserID, ActivityID, Comment, Date) VALUES (?, ?, ?, ?);`, [comment.userID, comment.activityID, comment.value, comment.date])
-    .then
-    (logger.info("Successfully created comnment [model]"))
-    .catch
-    ((error) => {
-    logger.error(new ERRORS.DatabaseWriteError(error));
+  await connection
+    .execute(
+      `INSERT INTO Comments (UserID, ActivityID, Comment, Date) VALUES (?, ?, ?, ?);`,
+      [comment.userID, comment.activityID, comment.value, comment.date]
+    )
+    .then(logger.info(`Successfully created comment \"${comment.value}\"`))
+    .catch((error) => {
+      logger.error(new ERRORS.DatabaseWriteError(error));
     });
-    
-    return comment;
+
+  return comment;
+}
+
+/**
+ * Delete a comment from the database
+ * @param {*} userID the id of the user who wrote the comment
+ * @param {*} activityID the id of the activity the comment is associated with
+ * @returns true if successful, false otherwise
+ */
+async function deleteComment(userID, activityID) {
+  // Get the connection to the database
+  let connection = DATABASES.getConnection();
+
+  // Remove the comment from the database
+  await connection
+    .execute(`DELETE FROM Comments WHERE UserID = ? AND ActivityID = ?;`, [
+      userID,
+      activityID,
+    ])
+    .then(logger.info("Successfully deleted comment"))
+    .catch((error) => {
+      logger.error(new ERRORS.DatabaseWriteError(error));
+      return false;
+    });
+
+  return true;
 }
 
 module.exports = {
-    createComment
+  createComment,
+  deleteComment,
 };
